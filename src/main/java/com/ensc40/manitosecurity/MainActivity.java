74c4ec4b0038442b928c859eddafccb24c7b1a24
Collin Walker <ioannes120@gmail.com>
2014-11-14 13:44:31 -0800

    big update

--- src/main/java/com/ensc40/manitosecurity/MainActivity.java
+++ src/main/java/com/ensc40/manitosecurity/MainActivity.java
@@ -1,30 +1,45 @@
 package com.ensc40.manitosecurity;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
-    @Override // hello world
+    private ImageButton imageState;
+    private SharedPreferences settings;
+    SharedPreferences.Editor editor;
+    String TAG = "MAINTAG";
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        //GET SHARED PREFERENCES and SET UP EDITOR
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+
+        editor = settings.edit();
+
+        //Set up finished
+        //if(!settings.getBoolean("setUp", false)) {setUpFinished();}
+
+        setUpUI();
+
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,25 +56,47 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_settings:
+                Intent intent = new Intent(getApplicationContext(), SetUpWifi.class);
+                startActivity(intent);
                 return true; //this is a comment
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+    private void setUpFinished(){
+        editor.putBoolean("setUp", true).commit();
     }
 
+    private void setUpUI(){
+        imageState = (ImageButton) findViewById(R.id.stateImage);
+
+
+        if (!settings.getBoolean("armState", false)){		//if setting is off, button should be off
+            imageState.setImageResource(R.drawable.button_off);
+        }
+
+        imageState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = settings.edit();
+                if(settings.getBoolean("armState", false) == true){			//if armed -> disarm
+                    imageState.setImageResource(R.drawable.button_off);
+                    editor.putBoolean("armState", false).commit();
+                }
+
+                else{   //disarm -> arm
+                    imageState.setImageResource(R.drawable.button_on);
+                    editor.putBoolean("armState", true).commit();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
 }
